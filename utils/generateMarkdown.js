73d83bf84678985ed39aeb10297f9a9d660116db
Kasey Jones <joneskm72@gmail.com>
2020-08-20 14:12:58 -0400

    Added more formatting

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,16 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Table of contents
+  * [License](#license)
+  * [Description](#description)
+  * [How to Install](#installation)
+  * [How to Use](#usage)
+  * [Testing](#testing)
+  * [Contributors](#contributors)
+  * [Questions?](#questions)
+  * [Deployed Application Link](#deployed-application-link)
+
 ## License:
 
 ![license](https://img.shields.io/badge/license-${data.license}-blue.svg)
@@ -32,6 +42,10 @@ function generateMarkdown(data) {
 ### ${data.link}
 
 ### ${data.email}
+
+## Deployed Application Link:
+
+### ${data.application-link}
 
 `;
 }
