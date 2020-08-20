ef46f5f9fe3dfe4284766035f1bca07807f329ad
Kasey Jones <joneskm72@gmail.com>
2020-08-20 14:06:22 -0400

    Updated license formatting

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,19 +2,30 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## License:
+
 ![license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+
+## Description:
 
 > ${data.description}
 
+## How to Install:
+
 > ${data.installation}
 
+## How to Use:
 > ${data.usage}
+
+## Testing:
 
 > ${data.tests}
 
+## Contributors:
+
 ### ${data.contributors}
 
-## Questions
+## Questions?
 
 ### ${data.username}
 
